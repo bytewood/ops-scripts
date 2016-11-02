4ce6a708dda966f11006e869f03949e5fa856d66
Peter Wood <peter@cowboyaliens.com>
2016-11-02 17:39:04 +0200

    Updated build and push script to only use registry based tags no more local tags as well

--- container-build.sh
+++ container-build.sh
@@ -5,12 +5,13 @@ folder=`dirname $0`
 dockerfile="$folder/Dockerfile"
 usage() {
     echo "Usage:"
-    echo "./docker-build.sh docker_image tag"
+    echo "./docker-build.sh <registry> <docker_image> <tag>"
     exit 1
 }
 
+[ -z "${registry}" ] && usage
 [ -z "${repository}" ] && usage
 [ -z "${tag}" ] && usage
 
-docker build -f "$dockerfile" -t ${repository}:${tag} -t ${repository}:latest .
+docker build -f "$dockerfile" -t "${registry}/${repository}:${tag}" -t "${registry}${repository}:latest" .
 
--- container-push.sh
+++ container-push.sh
@@ -9,7 +9,7 @@ usage() {
     echo "Usage:"
     echo "./docker-push.sh <registry> <repository> <tag>"
     echo "e.g."
-    echo "./docker-push.sh localhost:500 bytewood/ops/microservice 999"
+    echo "./docker-push.sh localhost:500 bytewood/ops-microservice 999"
     exit 1
 }
 
@@ -17,8 +17,5 @@ usage() {
 [ -z "${repository}" ] && usage
 [ -z "${tag}" ] && usage
 
-docker tag "${repository}:${tag}" "${registry}/${repository}:${tag}"
-docker tag "${repository}:latest" "${registry}/${repository}:latest"
 docker push "${registry}/${repository}:${tag}"
-docker push "${registry}/${repository}:latest"
 
